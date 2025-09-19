ab46c9495bd4db5cdf80b2141278636760832294
Ngoc Anh <nguyenthingocanh0855772567@gmail.com>
2025-09-20 01:18:30 +0700

    sửa lại bài 3

--- bai_3.java
+++ bai_3.java
@@ -1,11 +1,39 @@
-// Bài 3: Viết chương trình thực hiện các phép tính cơ bản (cộng, trừ, nhân, chia) dựa trên lựa chọn của người dùng.
-import java.util.scanner;
-class bai_1
+import java.util.Scanner;
+public class bai_3
 {
-    public static void main (string[]args)
+    public static void main(String[] args)
     {
-        Scanner sc=new scanner(system.in);
-        system.out.println("nhap phep toan ban muon: ");
-        string s=sc.nextline()
+        while(true)
+        {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("nhap phep toan ban muon: ");
+            String s = sc.nextLine();
+            System.out.print("nhap so thu nhat: ");
+            double a = sc.nextDouble();
+            System.out.print("nhap so thu hai: ");
+            double b = sc.nextDouble();
+            sc.nextLine();
+            switch(s)
+            {
+                case "+":
+                        System.out.println("ket qua la: "+(a+b));
+                        break;
+                case "-":
+                        System.out.println("ket qua la: "+(a-b));
+                        break;
+                case "*":
+                        System.out.println("ket qua la: "+(a*b));
+                        break;
+                case "/":
+                        if(b==0)
+                            System.out.println("loi phep tinh!");
+                        else
+                            System.out.println("ket qua la: "+(a/b));
+                        break;  
+                default:
+                        System.out.println("loi phep tinh!");
+                        break;         
+            }
+        }
     }
 }
