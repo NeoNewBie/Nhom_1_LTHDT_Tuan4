dc3c1870ef10de0afc9047f14b3704065048126c
Ngoc Anh <nguyenthingocanh0855772567@gmail.com>
2025-09-20 01:04:29 +0700

    upload bài 3 và bài 7

---
+++ bai_3.java
@@ -0,0 +1,11 @@
+// Bài 3: Viết chương trình thực hiện các phép tính cơ bản (cộng, trừ, nhân, chia) dựa trên lựa chọn của người dùng.
+import java.util.scanner;
+class bai_1
+{
+    public static void main (string[]args)
+    {
+        Scanner sc=new scanner(system.in);
+        system.out.println("nhap phep toan ban muon: ");
+        string s=sc.nextline()
+    }
+}
---
+++ bai_7.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class bai_7 
+{   
+    public static Scanner sc = new Scanner(System.in);
+    public static int []A;
+public static void Nhapmang(int n)
+{
+    A=new int[n];
+    for(int i=0; i<n; i++)
+    {
+        System.out.print("A["+i+"]=");
+            A[i]=sc.nextInt();
+    }
+}
+public static void Xuat(int n)
+{
+    System.out.println("Cac phan tu trong mang");
+    for(int i=0;i<n;i++)
+        System.out.println("A["+i+"]="+A[i]);
+}
+public static int Tinhtong(int n)
+{
+    int tong=0;
+    for(int i=0;i<n;i++)
+        tong+=A[i];
+    return tong;
+}
+public static void main(String[] args)
+{
+    System.out.print("Nhap so phan tu: ");
+    int n=sc.nextInt();
+    Nhapmang(n);
+    Xuat(n);
+    int tong= Tinhtong(n);
+    System.out.print("Tong cac phan tu trong mang la: "+tong);
+}
+}
