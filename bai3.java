261082a18a887f1f47e1744d9f04b893fa12e943
Ngoc Anh <nguyenthingocanh0855772567@gmail.com>
2025-09-23 15:59:26 +0700

    Thêm bài 3,7,9

---
+++ bai3.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class bai3
+{
+    public static void main(String[] args)
+    {
+        while(true)
+        {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("nhap phep toan ban muon: ");
+            String s = sc.nextLine();
+            System.out.print("nhap so thu nhat: ");
+            double a = sc.nextDouble();
+            System.out.print("nhap so thu hai: ");
+            double b = sc.nextDouble();
+            sc.nextLine();
+            switch(s)
+            {
+                case "+":
+                        System.out.println("ket qua la: "+(a+b));
+                        break;
+                case "-":
+                        System.out.println("ket qua la: "+(a-b));
+                        break;
+                case "*":
+                        System.out.println("ket qua la: "+(a*b));
+                        break;
+                case "/":
+                        if(b==0)
+                            System.out.println("loi phep tinh!");
+                        else
+                            System.out.println("ket qua la: "+(a/b));
+                        break;  
+                default:
+                        System.out.println("loi phep tinh!");
+                        break;         
+            }
+        }
+    }
+}
